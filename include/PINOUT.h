20a7aef3331f2289f5393ddb9dfcde71208c5238
Quinn <quinn.henthorne@gmail.com>
2024-05-16 17:59:53 -0400

    Updated micro configuration files to match the new micro we're using.

--- include/PINOUT.h
+++ include/PINOUT.h
@@ -6,39 +6,39 @@
 #pragma once
 
 // Stack 1 pins
-#define STACK1_ADC_PIN 34
-#define STACK1_LED_PIN 5
+#define STACK1_ADC_PIN 4
+#define STACK1_LED_PIN 9
 
 // Stack 2 pins
-#define STACK2_ADC_PIN 39
-#define STACK2_LED_PIN 18
+#define STACK2_ADC_PIN 5
+#define STACK2_LED_PIN 10
 
 // Stack 3 pins
-#define STACK3_ADC_PIN 36
-#define STACK3_LED_PIN 19
+#define STACK3_ADC_PIN 6
+#define STACK3_LED_PIN 11
 
 // Stack 4 pins
-#define STACK4_ADC_PIN 35
-#define STACK4_LED_PIN 17
+#define STACK4_ADC_PIN 7
+#define STACK4_LED_PIN 12
 
 // Stack 5 pins
-#define STACK5_ADC_PIN 32
-#define STACK5_LED_PIN 16
+#define STACK5_ADC_PIN 15
+#define STACK5_LED_PIN 13
 
 // Stack 6 pins
-#define STACK6_ADC_PIN 33
-#define STACK6_LED_PIN 4
+#define STACK6_ADC_PIN 16
+#define STACK6_LED_PIN 14
 
 // Stack 7 pins
-#define STACK7_ADC_PIN 25
-#define STACK7_LED_PIN 0
+#define STACK7_ADC_PIN 17
+#define STACK7_LED_PIN 37
 
 // Stack 8 pins
-#define STACK8_ADC_PIN 26
-#define STACK8_LED_PIN 2
+#define STACK8_ADC_PIN 18
+#define STACK8_LED_PIN 38
 
 // Stack 9 pins
-#define STACK9_ADC_PIN 27
-#define STACK9_LED_PIN 15
+#define STACK9_ADC_PIN 8
+#define STACK9_LED_PIN 45
 
 
